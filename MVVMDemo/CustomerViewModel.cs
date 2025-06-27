47771cf7a5393cf7540e854489af22f980983858
常志 佐々木 <sasaki.joji@d-dsn.co.jp>
2025-06-27 15:43:02 +0900

    プロジェクト ファイルを追加します。

---
+++ MVVMDemo/CustomerViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Data.Entity; // Ensure you have the Entity Framework package installed
+
+namespace MVVMDemo
+{
+    public class CustomerViewModel : IDisposable
+    {
+        private NorthwindEntities _db;
+        private bool _disposed;
+        public List<Customer> Customers { get; private set; }
+
+        // 修正: LoadCustomersAsync メソッドのアクセス修飾子を 'private' から 'public' に変更  
+        public async Task LoadCustomersAsync()
+        {
+            _db = new NorthwindEntities();
+            Customers = await Task.Run(() => _db.Customers.ToList());
+            INotifyPropertyChanged(nameof(Customers));
+        }
+
+        public void Dispose() { /* 既存のコード */ }
+        public void DeleteCustomer(Customer customer) { /* 既存のコード */ }
+        public void NewCustomer(Customer customer) { /* 既存のコード */ }
+        public void UpdateCustomer(Customer customer) { /* 既存のコード */ }
+        public void SaveChanges() { /* 既存のコード */ }
+        protected virtual void Dispose(bool disposing) { /* 既存のコード */ }
+        public void INotifyPropertyChanged(string propertyName) { /* 既存のコード */ }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }      
+}
---
+++ MVVMDemo/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MVVMDemo
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private CustomerViewModel vm;
+
+
+        /// <summary>
+        /// default コンストラクター
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            vm = new CustomerViewModel();
+            this.DataContext = vm;
+
+            // 非同期でデータをロードする
+            await vm.LoadCustomersAsync();
+
+            // ObservableCollectionを直接ItemsSourceにバインドする
+            this.ClientDataGrid.ItemsSource = vm.Customers;
+
+            System.Windows.Data.CollectionViewSource customerViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("customerViewSource")));
+            // 必要に応じて、CollectionViewSourceを設定する
+            customerViewSource.Source = vm.Customers;
+        }
+
+    }
+}
